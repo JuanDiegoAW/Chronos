5f9bc08c7599c2164eda19909c522f448683b8aa
chris <caqp14@gmail.com>
2019-10-31 22:20:40 -0600

    Se agrega formulario pago evento

---
+++ app/src/main/java/com/example/login/Activities/PagoEvento.kt
@@ -0,0 +1,13 @@
+package com.example.login.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.login.R
+
+class PagoEvento : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pago_evento)
+    }
+}
