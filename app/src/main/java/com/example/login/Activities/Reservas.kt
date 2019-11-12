b980b16a7bf607c5c967fe36015a3ca1a7d9e619
Oswaldo Alvarez <mynor4755@hotmail.com>
2019-11-12 03:00:37 -0600

    1.-Se arreglo mostrar eventos, Ahora ya solo hay que páginar 2.-Se muestran las diferentes páginas de los asientos 3.-Aún pendientes de reservar 4.-se agregaron botones y textview en el activity_reservas

--- app/src/main/java/com/example/login/Activities/Reservas.kt
+++ app/src/main/java/com/example/login/Activities/Reservas.kt
@@ -1,20 +1,21 @@
 package com.example.login.Activities
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
 import com.example.login.Clases.*
-import com.example.login.Clases.AdaptadorButtonsOnClickListener
+import com.example.login.R
 import kotlinx.android.synthetic.main.activity_reservas.*
 import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
-import android.view.View
-import android.widget.*
-import com.example.login.R
-import java.util.ArrayList
+import java.util.*
 
 
 class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
@@ -26,6 +27,12 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
     private var arrayBotonera: ArrayList<LinearLayout> = ArrayList()
     private var asientoId : HashMap<Button,Int> = hashMapOf()
     private var infoAsientos :ArrayList<AsientosDatos> = ArrayList()
+    private var locaidad=0
+    private var asientosDisponibles=0
+    private var asientosTotales=0
+    private var siguiente="null"
+    private var anterior="null"
+    private var precio=0.0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +50,13 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
         btnReserva.setOnClickListener {
             AdaptadorCuadroDialogo(this,asientoId)
         }
+        btnSiguiente.setOnClickListener {
+            getAsientosLocalidad(this.siguiente)
+        }
+        btnAnterior.setOnClickListener {
+            getAsientosLocalidad(this.anterior)
+        }
+
     }
 
     private fun obtenerLocalidad(eventoCod:String){
@@ -81,9 +95,7 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
             servicio.desconectar()
         }
     }
-    override fun onNothingSelected(p0: AdapterView<*>?) {
-        println(":V")
-    }
+    override fun onNothingSelected(p0: AdapterView<*>?) {}
 
     /**
      * Sobreescribir el evento de selección de un spinner para crear dinámicamente los botones.
@@ -93,53 +105,14 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
         if (p0 != null) {
             var jsonObject = servicio.metodoGetBusqueda("localidad","codigoEventos="+adaptador_evento.getEvento().codigo
                     +"&idTipoLocalidad="+ datosLocalidad[p0.selectedItem])
-            var cantidad = jsonObject.optInt("cantidadAsientos")
-            var cant = cantidad
-            var contador=1
+            this.asientosTotales = jsonObject.optInt("cantidadAsientos")
+            this.precio=jsonObject.optDouble("costo")
+            this.asientosDisponibles=jsonObject.optInt("cantidadAsientosDisponible")
             this.infoAsientos = ArrayList()
-            getAsientosLocalidad(jsonObject.optInt("id"))
-            txtCantidadAsientos.text="Cantidad asientos: ${jsonObject.optString("cantidadAsientos")}"
-            txtAsientosDisponibles.text="Asientos disponibles: ${jsonObject.optString("cantidadAsientosDisponible")}"
-            if (llPrincipal.childCount>0){
-                llPrincipal.removeAllViews()
-            }
-            this.arrayBotonera = ArrayList()
-            cantidad = if ((cantidad%5)>0) (cantidad/5) +1 else cantidad/5
-            for (i in 0 until cantidad){
-                this.arrayBotonera.add(LinearLayout(this))
-                for (j in 0 until 5){
-                    if (contador<=cant){
-                        val button = Button(this)
-                        button.textSize = 15F
-                        button.setTextColor(Color.WHITE)
-                        if (contador<=this.infoAsientos.size){
-                            button.text = this.infoAsientos[contador-1].getNumeroAsiento()
-
-                            if (!this.infoAsientos[contador-1].isDisponible()){
-                                button.isEnabled=this.infoAsientos[contador-1].isDisponible()
-                                button.setBackgroundColor(Color.rgb(253,127,72))
-                            }else{
-                                button.setBackgroundColor(Color.rgb(251,74,86))
-                            }
-                            button.setOnClickListener(AdaptadorButtonsOnClickListener(this,
-                                this.asientoId,this.infoAsientos[contador-1].getIdAsiento()))
-                        }else {
-                            button.text = contador.toString()
-                            button.setBackgroundColor(Color.rgb(251, 74, 86))
-                            button.setOnClickListener(
-                                AdaptadorButtonsOnClickListener(
-                                    this,
-                                    this.asientoId, contador
-                                )
-                            )
-                        }
-                        this.arrayBotonera[i].addView(button)
-                    }else
-                        break
-                    contador ++
-                }
-                llPrincipal.addView(this.arrayBotonera[i])
-            }
+            txtCantidadAsientos.text ="Asientos totales: ${this.asientosTotales}"
+            txtPrecio.text="Precio: ${this.precio}"
+            txtAsientosDisponibles.text="Asientos disponibles: ${this.asientosDisponibles}"
+            getAsientosLocalidad("http://edvfelipe.pythonanywhere.com/api/asientos/?idLocalidad=${jsonObject.optInt("id")}")
         }
     }
 
@@ -149,11 +122,11 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
      * De ello tomo el id para hacer el cambio, el numero del asiento para ponerlo como texto al boton
      * disponibilidad para deshabilitar/habilitar el boton
      */
-    private fun getAsientosLocalidad(idLocalidad:Int){
+    private fun getAsientosLocalidad(ruta:String){
         try
         {
-            var entrada = BufferedReader(InputStreamReader(this.servicio.metodoGetBusquedaArray
-                ("asientos", "idLocalidad=$idLocalidad")))
+            var entrada = BufferedReader(InputStreamReader(this.servicio.metodoGetPaginacion
+                (ruta)))
             var respuesta = StringBuffer()
             //Ciclo para ir leyendo línea por línea e ir agregarlo en respuesta
             var linea : String?
@@ -167,13 +140,33 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
             val json: String
             //paso a un string el json que tengo para posteriormente manipularlo
             json = respuesta.toString()
-            val arrayJson = JSONArray(json)
+            val jsonObject = JSONObject(json)
+            this.siguiente=jsonObject.optString("next")
+            this.anterior=jsonObject.optString("previous")
+            btnSiguiente.isEnabled=this.siguiente!="null"
+            btnAnterior.isEnabled= this.anterior != "null"
+            val arrayJson = JSONArray(jsonObject.optString("results"))
+            //Limpio el layout principal cada vez que voy a paginar
+            if (llPrincipal.childCount>0){
+                llPrincipal.removeAllViews()
+            }
+            this.arrayBotonera = ArrayList()
+            var contador=0
+            var contador1=0
+            println(arrayJson.length())
             for (i in 0 until arrayJson.length()) {
                 var objetos=arrayJson.getJSONObject(i)
-                this.infoAsientos.add(AsientosDatos(objetos.optBoolean("disponible"),
-                    objetos.optString("numeroAsiento"),
-                    objetos.optInt("id")))
-        }
+                this.arrayBotonera.add(LinearLayout(this))
+                if (contador1!=4){
+                    insertarButton(objetos,contador)
+                    contador1++
+                }else {
+                    insertarButton(objetos,contador)
+                    llPrincipal.addView(this.arrayBotonera[contador])
+                    contador++
+                    contador1=0
+                }
+            }
             entrada.close()
         }
         catch (e: IOException) {
@@ -185,6 +178,23 @@ class Reservas() : AppCompatActivity(),AdapterView.OnItemSelectedListener {
         finally {
             servicio.desconectar()
         }
+    }
+
+    private fun insertarButton(objeto:JSONObject,contador:Int){
+        val button = Button(this)
+        button.textSize = 15F
+        //button.setTextColor(Color.WHITE)
+        button.text=objeto.getString("numeroAsiento")
+        val estado=objeto.optBoolean("disponible")
+        if (!estado){
+            button.isEnabled=estado
+            button.setBackgroundColor(Color.rgb(253,127,72))
+        }else{
+            button.setBackgroundColor(Color.rgb(251,74,86))
+        }
+        button.setOnClickListener(AdaptadorButtonsOnClickListener(this,
+            this.asientoId,objeto.getInt("id")))
+        this.arrayBotonera[contador].addView(button)
     }
     override fun onBackPressed() {
         if (asientoId.isNotEmpty()){
